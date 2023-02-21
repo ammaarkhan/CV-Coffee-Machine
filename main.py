0635f1023fc5d7270454d7699d9bc704dcd20ca6
ammaarkhan <ammaarkhan03@gmail.com>
2023-02-20 22:43:05 -0800

    add background image to feed

--- main.py
+++ main.py
@@ -5,7 +5,16 @@ cap = cv2.VideoCapture(0)
 cap.set(3, 640)
 cap.set(4, 480)
 
+imgBackground = cv2.imread("Resources/Background.png")
+
 while True:
     success, img = cap.read()
-    cv2.imshow("Image", img)
+    # this will overlay the camera feed on the image
+    imgBackground[139:139+480, 50:50+640] = img
+
+    # displaying
+    # cv2.imshow("Image", img)
+    cv2.imshow("Background", imgBackground)
     cv2.waitKey(1)
+
+
